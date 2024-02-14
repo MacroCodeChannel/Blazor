ec932f561a78580a3f7c782b608684fc8f2fb39f
Obadiah Korir <korir400@gmail.com>
2024-02-14 19:47:24 +0300

    Image Uploads

--- StudentsManagement/obj/Debug/net8.0/StudentsManagement.AssemblyInfo.cs
+++ StudentsManagement/obj/Debug/net8.0/StudentsManagement.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentsManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab710d1ecbc3a7779a4a436b75179f7b32acba0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d80e124a21a542e72f258c78fe5764264f6761")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentsManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentsManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
